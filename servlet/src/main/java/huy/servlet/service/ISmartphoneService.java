b892dea6d89d7bbdad54d0b7709cce4daaea6fa4
AnHuy <92672519+danganhuy@users.noreply.github.com>
2025-01-23 11:05:34 +0700

    Finished

---
+++ servlet/src/main/java/huy/servlet/service/ISmartphoneService.java
@@ -0,0 +1,15 @@
+package huy.servlet.service;
+
+import huy.servlet.model.Smartphone;
+
+import java.util.Optional;
+
+public interface ISmartphoneService {
+    Iterable<Smartphone> findAll();
+
+    Optional<Smartphone> findById(Long id);
+
+    Smartphone save(Smartphone smartPhone);
+
+    void remove(Long id);
+}
